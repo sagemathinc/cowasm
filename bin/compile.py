70554c863dda50cf9adf498076e2deb2091480c1
William Stein <wstein@sagemath.com>
2021-09-08 22:59:19 +0000

    adding ntl

---
+++ bin/compile.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import subprocess, sys
+
+def build(compiler):
+    args = ["zig", compiler, "-target", "wasm32-wasi"]
+    prev = None
+    make_exe = False
+    for x in sys.argv[1:]:
+        if prev == '-o':
+            args.append(x + '.wasm')
+            make_exe = x
+        else:
+            args.append(x)
+        prev = x
+    print(' '.join(args))
+    subprocess.run(args, capture_output=True, check=True)
+    if make_exe:
+        file = open(make_exe, 'w')
+        file.write(f"""
+#!/usr/bin/env bash
+wasmer run {make_exe}.wasm --dir . -- "$@"
+""")
+        file.close()
+        subprocess.run(["chmod", "+x", make_exe], capture_output=True, check=True)
+        # This might be nice, but it is (1) slow, (2) big, and (3) I can't
+        # figure how to give it filesystem access.
+        #args = ["wasmer", "create-exe", make_exe + '.wasm', "-o", make_exe]
+        #print(' '.join(args))
+        #print(subprocess.run(args, capture_output=True, check=True))
+
