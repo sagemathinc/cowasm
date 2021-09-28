53203c1d8f533d4d6db6925427ccc5813891fd7d
William Stein <wstein@sagemath.com>
2021-09-28 20:21:10 +0000

    write version of trial division factoring with no dynamic memory allocation

--- lib/src/factor.zig
+++ lib/src/factor.zig
@@ -20,8 +20,6 @@ fn Expand(comptime T: type, F: Factorization(T)) !T {
     return N;
 }
 
-// TODO: redo in a way that doesn't require dynamic allocation by using
-// if (@typeInfo(@TypeOf(N)).Int.bits > 128) {unreachable;}
 pub fn factorTrialDivision(allocator: *std.mem.Allocator, N: anytype) !Factorization(@TypeOf(N)) {
     const T = @TypeOf(N);
     var v = Factorization(T).init(allocator);
@@ -45,7 +43,110 @@ pub fn factorTrialDivision(allocator: *std.mem.Allocator, N: anytype) !Factoriza
     return v;
 }
 
+// Version of factoring for numbers up to 128 bits that doesn't use any
+// dynamic allocation.
+// The product 2 * 3 * ... * 103 of first 27 primes is bigger than 2^128,
+// but the product 2 * 3 * ... * 101 is less than 2^128.  Thus the number
+// of prime divisors of a number less than 2^128 is at most 26.
+// Similarly for 64-bit: at most 15 primes.
+// For 32-bit: at most 9 primes.
+// For 16-bit: at most 6 primes.
+// Also the exponent of any prime power divisor fits in a u8, obviously.
+// This wastes some memory, but doesn't require any dynamic heap allocation.
+// NOTE: our trial division implementation is way too slow to be useful
+// for most numbers near 128 bits!
+fn SmallPrimePower(comptime T: type) type {
+    return struct { p: T, e: u8 };
+}
+
+fn SmallFactorization(comptime T: type) type {
+    const bits = @typeInfo(T).Int.bits;
+    comptime var FACTORS: comptime_int = undefined;
+    if (bits > 128) { // so it won't even compile
+        unreachable;
+    } else if (bits > 64) {
+        FACTORS = 26;
+    } else if (bits > 32) {
+        FACTORS = 15;
+    } else if (bits > 16) {
+        FACTORS = 9;
+    } else {
+        FACTORS = 6;
+    }
+
+    return struct {
+        const Fact = @This();
+
+        factors: [FACTORS]SmallPrimePower(T),
+        len: u8,
+
+        pub fn init(N: T) Fact {
+            var factors: [FACTORS]SmallPrimePower(T) = undefined;
+            var M = N;
+            var len: u8 = 0;
+            while (M > 1) {
+                //std.debug.print("find factor of M={}\n", .{M});
+                const p = trialDivision(M);
+                //std.debug.print("got p={}\n", .{p});
+                if (p == M) {
+                    // M is prime
+                    factors[len] = SmallPrimePower(T){ .p = p, .e = 1 };
+                    len += 1;
+                    break;
+                } else {
+                    // p is a proper prime division of M.
+                    var e: u8 = 1;
+                    M = @divExact(M, p);
+                    while (@mod(M, p) == 0) : (M = @divExact(M, p)) {
+                        e += 1;
+                    }
+                    factors[len] = SmallPrimePower(T){ .p = p, .e = e };
+                    len += 1;
+                }
+            }
+            //std.debug.print("factors = {}\n", .{FACTORS});
+            return Fact{ .factors = factors, .len = len };
+        }
+
+        pub fn expand(self: Fact) !T {
+            var N: T = 1;
+            for (self.factors[0..self.len]) |factor| {
+                // std.debug.print("p={},e={}\n", .{ factor.p, factor.e });
+                var i: u8 = 0;
+                while (i < factor.e) : (i += 1) {
+                    N *= factor.p;
+                }
+            }
+            return N;
+        }
+    };
+}
+
+pub fn smallFactor(N: anytype) SmallFactorization(@TypeOf(N)) {
+    return SmallFactorization(@TypeOf(N)).init(N);
+}
+
 const expect = std.testing.expect;
+
+test "small factorization of some numbers" {
+    const N: i32 = 27 * 25 * 389 * 389;
+    const F = smallFactor(N);
+    try expect((try F.expand()) == N);
+}
+
+// slightly slow
+test "small factorization of a greater than 64- bit number" {
+    const N: u128 = 36893488147419103242; // = 2^65 + 10.
+    const F = smallFactor(N);
+    try expect((try F.expand()) == N);
+    try expect(F.factors.len == 26);
+}
+
+test "small factorization of tiny number" {
+    const N: i16 = 16385; // = 2^14 + 1
+    const F = smallFactor(N);
+    try expect((try F.expand()) == N);
+}
 
 test "factoring an explicit number" {
     const N: i32 = 27 * 25 * 389 * 389;
