cb544e6fb5469eba3eef12feda2335747b378821
William Stein <wstein@sagemath.com>
2021-10-08 23:32:56 +0000

    make output/modules.py proper python syntax

--- packages/jpython/src/output/modules.py
+++ packages/jpython/src/output/modules.py
@@ -16,10 +16,12 @@ def write_imports(module, output):
     imports = []
     for import_id in Object.keys(module.imports):
         imports.push(module.imports[import_id])
-    imports.sort(def(a, b):
+
+    def compare(a, b):
         a, b = a.import_order, b.import_order
         return -1 if a < b else (1 if a > b else 0)
-    )
+
+    imports.sort(compare)
     if imports.length > 1:
         output.indent()
         output.print('var ρσ_modules = {};')
@@ -46,13 +48,15 @@ def write_imports(module, output):
                 output.print('ρσ_modules.' + module_id)
             else:
                 output.print('ρσ_modules["' + module_id + '"]')
-            output.space(), output.print('='), output.space(), output.print('{}')
+            output.space(), output.print('='), output.space(), output.print(
+                '{}')
             output.end_statement()
 
     # Output module code
     for module_ in imports:
         if module_.module_id is not '__main__':
             print_module(module_, output)
+
 
 def write_main_name(output):
     if output.options.write_name:
@@ -63,13 +67,15 @@ def write_main_name(output):
         output.newline()
         output.newline()
 
+
 def declare_exports(module_id, exports, output, docstrings):
     seen = {}
     if output.options.keep_docstrings and docstrings and docstrings.length:
-        exports.push({'name':'__doc__', 'refname': 'ρσ_module_doc__'})
+        exports.push({'name': '__doc__', 'refname': 'ρσ_module_doc__'})
         output.newline(), output.indent()
         v = 'var'
-        output.assign(v + ' ρσ_module_doc__'), output.print(JSON.stringify(create_doctring(docstrings)))
+        output.assign(v + ' ρσ_module_doc__'), output.print(
+            JSON.stringify(create_doctring(docstrings)))
         output.end_statement()
     output.newline()
     for symbol in exports:
@@ -79,9 +85,11 @@ def declare_exports(module_id, exports, output, docstrings):
                 output.print('ρσ_modules.' + module_id + '.' + symbol.name)
             else:
                 output.print('ρσ_modules["' + module_id + '"].' + symbol.name)
-            output.space(), output.print('='), output.space(), output.print(symbol.refname or symbol.name)
+            output.space(), output.print('='), output.space(), output.print(
+                symbol.refname or symbol.name)
             seen[symbol.name] = True
             output.end_statement()
+
 
 def prologue(module, output):
     # any code that should appear before the main body
@@ -90,20 +98,34 @@ def prologue(module, output):
     output.indent()
     v = 'var'
     output.print(v), output.space()
-    output.spaced.apply(output, (('ρσ_iterator_symbol = (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") ? Symbol.iterator : "iterator-Symbol-5d0927e5554349048cf0e3762a228256"'.split(' '))))
+    output.spaced.apply(output, ((
+        'ρσ_iterator_symbol = (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") ? Symbol.iterator : "iterator-Symbol-5d0927e5554349048cf0e3762a228256"'
+        .split(' '))))
     output.end_statement()
     output.indent(), output.print(v), output.space()
-    output.spaced.apply(output, (('ρσ_kwargs_symbol = (typeof Symbol === "function") ? Symbol("kwargs-object") : "kwargs-object-Symbol-5d0927e5554349048cf0e3762a228256"'.split(' '))))
+    output.spaced.apply(output, ((
+        'ρσ_kwargs_symbol = (typeof Symbol === "function") ? Symbol("kwargs-object") : "kwargs-object-Symbol-5d0927e5554349048cf0e3762a228256"'
+        .split(' '))))
     output.end_statement()
-    output.indent(), output.spaced('var', 'ρσ_cond_temp,', 'ρσ_expr_temp,', 'ρσ_last_exception'), output.end_statement()
-    output.indent(), output.spaced('var', 'ρσ_object_counter', '=', '0'), output.end_statement()
+    output.indent(), output.spaced(
+        'var', 'ρσ_cond_temp,', 'ρσ_expr_temp,',
+        'ρσ_last_exception'), output.end_statement()
+    output.indent(), output.spaced('var', 'ρσ_object_counter', '=',
+                                   '0'), output.end_statement()
     # Needed for Chrome < 51 and Edge as of August 2016
-    output.indent(), output.spaced('if(', 'typeof', 'HTMLCollection', '!==', '"undefined"', '&&', 'typeof', 'Symbol', '===', '"function")',
-        'NodeList.prototype[Symbol.iterator]', '=', 'HTMLCollection.prototype[Symbol.iterator]', '=', 'NamedNodeMap.prototype[Symbol.iterator]', '=', 'Array.prototype[Symbol.iterator]')
+    output.indent(), output.spaced(
+        'if(', 'typeof', 'HTMLCollection', '!==', '"undefined"', '&&',
+        'typeof', 'Symbol', '===', '"function")',
+        'NodeList.prototype[Symbol.iterator]', '=',
+        'HTMLCollection.prototype[Symbol.iterator]', '=',
+        'NamedNodeMap.prototype[Symbol.iterator]', '=',
+        'Array.prototype[Symbol.iterator]')
     output.end_statement()
     # output the baselib
     if not output.options.baselib_plain:
-        raise ValueError('The baselib is missing! Remember to set the baselib_plain field on the options for OutputStream')
+        raise ValueError(
+            'The baselib is missing! Remember to set the baselib_plain field on the options for OutputStream'
+        )
     output.print(output.options.baselib_plain)
     output.end_statement()
 
@@ -116,13 +138,16 @@ def print_top_level(self, output):
         if is_main and output.options.keep_docstrings and self.docstrings and self.docstrings.length:
             output.newline(), output.indent()
             v = 'var'
-            output.assign(v + ' ρσ_module_doc__'), output.print(JSON.stringify(create_doctring(self.docstrings)))
+            output.assign(v + ' ρσ_module_doc__'), output.print(
+                JSON.stringify(create_doctring(self.docstrings)))
             output.end_statement()
 
     if output.options.private_scope and is_main:
-        output.with_parens(def():
+
+        def f_main_function():
             output.print("function()")
-            output.with_block(def():
+
+            def f_full_function():
                 # strict mode is more verbose about errors, and less forgiving about them
                 # kind of like Python
                 output.indent()
@@ -133,9 +158,11 @@ def print_top_level(self, output):
                 write_imports(self, output)
                 output.newline()
                 output.indent()
-                output.with_parens(def():
+
+                def f_function():
                     output.print("function()")
-                    output.with_block(def():
+
+                    def f_body():
                         write_main_name(output)
                         output.newline()
                         declare_vars(self.localvars, output)
@@ -146,12 +173,17 @@ def print_top_level(self, output):
                             output.indent()
                             output_comments(self.comments_after, output)
                             output.newline()
-                    )
-                )
+
+                    output.with_block(f_body)
+
+                output.with_parens(f_function)
+
                 output.print("();")
                 output.newline()
-            )
-        )
+
+            output.with_block(f_full_function)
+
+        output.with_parens(f_main_function)
         output.print("();")
         output.print("")
     else:
@@ -166,6 +198,7 @@ def print_top_level(self, output):
             output_comments(self.comments_after, output)
     set_module_name()
 
+
 def print_module(self, output):
     set_module_name(self.module_id)
 
@@ -176,10 +209,11 @@ def print_module(self, output):
 
     output.newline()
     output.indent()
-    output.with_parens(def():
+
+    def f_print_module():
         output.print("function()")
-        output.with_block(def():
-            # dump the logic of this module
+
+        def dump_the_logic_of_this_module():
             print_comments(self, output)
             if output.options.write_name:
                 output.indent()
@@ -188,50 +222,79 @@ def print_module(self, output):
                 output.print('"' + self.module_id + '"')
                 output.semicolon()
                 output.newline()
+
             def output_key(beautify, keep_docstrings):
                 return 'beautify:' + beautify + ' keep_docstrings:' + keep_docstrings
-            okey = output_key(output.options.beautify, output.options.keep_docstrings)
+
+            okey = output_key(output.options.beautify,
+                              output.options.keep_docstrings)
             if self.is_cached and okey in self.outputs:
                 output.print(self.outputs[okey])
             else:
                 output_module(output)
                 if self.srchash and self.filename:
                     cached = {
-                        'version':get_compiler_version(), 'signature':self.srchash, 'classes': {}, 'baselib':self.baselib,
-                        'nonlocalvars':self.nonlocalvars, 'imported_module_ids':self.imported_module_ids, 'exports':[],
-                        'outputs':{}, 'discard_asserts': bool(output.options.discard_asserts)
+                        'version': get_compiler_version(),
+                        'signature': self.srchash,
+                        'classes': {},
+                        'baselib': self.baselib,
+                        'nonlocalvars': self.nonlocalvars,
+                        'imported_module_ids': self.imported_module_ids,
+                        'exports': [],
+                        'outputs': {},
+                        'discard_asserts': bool(output.options.discard_asserts)
                     }
                     for cname in Object.keys(self.classes):
                         cobj = self.classes[cname]
-                        cached.classes[cname] = {'name':{'name':cobj.name.name}, 'static':cobj.static, 'bound':cobj.bound, 'classvars':cobj.classvars}
+                        cached.classes[cname] = {
+                            'name': {
+                                'name': cobj.name.name
+                            },
+                            'static': cobj.static,
+                            'bound': cobj.bound,
+                            'classvars': cobj.classvars
+                        }
                     for symdef in self.exports:
-                        cached.exports.push({'name':symdef.name})
+                        cached.exports.push({'name': symdef.name})
                     for beautify in [True, False]:
                         for keep_docstrings in [True, False]:
                             co = OutputStream({
-                                'beautify':beautify, 'keep_docstrings':keep_docstrings,
-                                'write_name':False, 'discard_asserts':output.options.discard_asserts
+                                'beautify':
+                                beautify,
+                                'keep_docstrings':
+                                keep_docstrings,
+                                'write_name':
+                                False,
+                                'discard_asserts':
+                                output.options.discard_asserts
                             })
-                            co.with_indent(output.indentation(), def():
-                                output_module(co)
-                            )
+                            co.with_indent(output.indentation(),
+                                           lambda: output_module(co))
                             raw = co.get()
-                            cached.outputs[output_key(beautify, keep_docstrings)] = raw
-                    cached_name = cache_file_name(self.filename, output.options.module_cache_dir)
+                            cached.outputs[output_key(beautify,
+                                                      keep_docstrings)] = raw
+                    cached_name = cache_file_name(
+                        self.filename, output.options.module_cache_dir)
                     try:
                         if cached_name:
-                            writefile(cached_name, JSON.stringify(cached, None, '\t'))
+                            writefile(cached_name,
+                                      JSON.stringify(cached, None, '\t'))
                     except Error as e:
-                        console.error('Failed to write output cache file:', cached_name, 'with error:', e)
-        )
-    )
+                        console.error('Failed to write output cache file:',
+                                      cached_name, 'with error:', e)
+
+        output.with_block(dump_the_logic_of_this_module)
+
+    output.with_parens(f_print_module)
     output.print("()")
     output.semicolon()
     output.newline()
     set_module_name()
 
+
 def print_imports(container, output):
     is_first_aname = True
+
     def add_aname(aname, key, from_import):
         nonlocal is_first_aname
         if is_first_aname:
@@ -264,7 +327,7 @@ def print_imports(container, output):
                     if i is 0:
                         add_aname(part, part, False)
                     else:
-                        q = parts[:i+1].join('.')
+                        q = parts[:i + 1].join('.')
                         output.indent()
                         output.spaced(q, '=', 'ρσ_modules["' + q + '"]')
                         output.end_statement()
