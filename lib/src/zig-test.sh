7f3f38f8a13913fac137c3e6f3720632fc557814
William Stein <wstein@sagemath.com>
2022-06-27 03:24:58 +0000

    upgdate some scripts for python instead of gmp/pari/mpfr

--- lib/src/zig-test.sh
+++ lib/src/zig-test.sh
@@ -3,12 +3,9 @@
 
 export SRC="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 export PACKAGES="$SRC"/../../packages
-export GMP_NATIVE="$PACKAGES"/gmp/dist/native
-export PARI_NATIVE="$PACKAGES"/pari/dist/native
-export MPFR_NATIVE="$PACKAGES"/mpfr/dist/native
-
-export DYLD_LIBRARY_PATH="$GMP_NATIVE/lib":"$PARI_NATIVE/lib":"$MPFR_NATIVE/lib"
-export LD_LIBRARY_PATH="$GMP_NATIVE/lib":"$PARI_NATIVE/lib":"$MPFR_NATIVE/lib"
+export PYTHON_NATIVE="$PACKAGES"/python/dist/native
+export DYLD_LIBRARY_PATH="$PYTHON_NATIVE/lib"
+export LD_LIBRARY_PATH="$PYTHON_NATIVE/lib"
 export ZIG_SYSTEM_LINKER_HACK=1
 
 #echo $DYLD_LIBRARY_PATH
