03e10a57a8ee30d62fb6fd64aa71841937927402
William Stein <wstein@sagemath.com>
2022-07-03 18:27:13 +0000

    building python on wasm -- getting past emscripten.h include issue, mainly; also more threading.py hacks for native python

--- packages/wasm-posix/src/emscripten.h
+++ packages/wasm-posix/src/emscripten.h
@@ -1,1 +1,19 @@
-/* This is needed since Python's official build system imports it. */
+/*
+Compatibility for things that assume they are building against emscripten,
+e.g., Python.
+*/
+
+#pragma once
+
+#define EM_IMPORT(NAME)
+
+#define _EM_JS(ret, c_name, js_name, params, code)                          \
+  ret c_name params EM_IMPORT(js_name);                                     \
+  EMSCRIPTEN_KEEPALIVE                                                      \
+  __attribute__((section("em_js"), aligned(1))) char __em_js__##js_name[] = \
+      #params "<::>" code;                                                  \
+
+#define EM_JS(ret, name, params, ...) \
+  _EM_JS(ret, name, name, params, #__VA_ARGS__)
+
+#define EMSCRIPTEN_KEEPALIVE
