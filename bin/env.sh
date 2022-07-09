31e18bc2ff4f607c76d30d3258d43b32e2ad93a5
William Stein <wstein@sagemath.com>
2022-07-09 09:20:33 -0700

    sworkaround this macos security issue to make the native build of python fully build without any errors:  https://developer.apple.com/forums/thread/9233

--- bin/env.sh
+++ bin/env.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 PACKAGES=`pwd`/packages
+
+export DYLD_LIBRARY_PATH="$PACKAGES"/cpython/dist/native/lib:"$PACKAGES"/zlib/dist/native/lib:$DYLD_LIBRARY_PATH
+export LD_LIBRARY_PATH="$PACKAGES"/cpython/dist/native/lib:"$PACKAGES"/zlib/dist/native/lib:$LD_LIBRARY_PATH
 export PATH=`pwd`/bin:"$PACKAGES"/zig/dist:"$PACKAGES"/cpython/dist/native/bin:$PATH
