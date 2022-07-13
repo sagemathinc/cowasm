39ae7e0243a03d16006dee7607befa02c421249d
William Stein <wstein@sagemath.com>
2022-07-12 22:29:23 -0700

    terminal - very basic evaluation with a minimal useful echo

--- packages/terminal/src/index.ts
+++ packages/terminal/src/index.ts
@@ -1,5 +1,6 @@
 import "xterm/css/xterm.css";
 import { Terminal } from "xterm";
+import LocalEchoController from "local-echo";
 import python from "python-wasm";
 import setTheme from "./theme";
 (window as any).python = python;
@@ -9,14 +10,32 @@ async function main() {
 
   const element = document.createElement("div");
   document.body.appendChild(element);
+  document.body.style.margin = '0px';
 
-  const terminal = new Terminal();
-  terminal.resize(120, 40);
-  setTheme(terminal, 'solarized-light');
-  terminal.open(element);
+  const term = new Terminal();
+  term.open(element);
+  // @ts-ignore
+  element.children[0].style.padding = '15px';
+  const localEcho = new LocalEchoController(term, { historySize: 200 });
+
+  term.resize(120, 40);
+  setTheme(term, "solarized-light");
   python.exec("import sys");
-  terminal.write(python.repr("sys.version"));
+  localEcho.println("Python " + python.repr("sys.version").slice(1, -1));
+  localEcho.println("Type code to execute.  Currently the only thing output is whatever is explicitly assigned to _ (underscore).")
+  localEcho.println("This is because writing to a file (e.g., /dev/stdout) doesn't work yet, due to encoding issues.")
 
+  // TODO: https://github.com/wavesoft/local-echo#addautocompletehandlercallback-args
+
+  while (true) {
+    try {
+      const input = await localEcho.read(">>> ");
+      python.exec(input);
+      localEcho.println(python.repr("_"));
+    } catch (err) {
+      term.write(`ERROR: ${err}\r\n`);
+    }
+  }
 }
 
 main();
