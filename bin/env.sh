9ab262ab4e5426735c393183301c03b6c4cea6eb
William Stein <wstein@sagemath.com>
2022-07-27 05:29:15 -0700

    improve bin/env.sh: add WABT; also, do NOT mess with native libs or native python, which we almost never need except for bootstrapping the build.

--- bin/env.sh
+++ bin/env.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 PACKAGES=`pwd`/packages
 
-export DYLD_LIBRARY_PATH="$PACKAGES"/cpython/dist/native/lib:"$PACKAGES"/zlib/dist/native/lib:$DYLD_LIBRARY_PATH
-export LD_LIBRARY_PATH="$PACKAGES"/cpython/dist/native/lib:"$PACKAGES"/zlib/dist/native/lib:$LD_LIBRARY_PATH
-export PATH=`pwd`/bin:"$PACKAGES"/zig/dist:"$PACKAGES"/cpython/dist/native/bin:$PATH
+# You have to run this via "source bin/env.sh" from the top level. It adds to your PATH:
+# - The scripts in this bin directory
+# - zig
+# - some scripts in python-wasm; in particular, the super useful wabt tools such as wasm-objdump and wasm-strip.
+
+export PATH=`pwd`/bin:"$PACKAGES"/zig/dist:"$PACKAGES"/python-wasm/node_modules/.bin:$PATH
