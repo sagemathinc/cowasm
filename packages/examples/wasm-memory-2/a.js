464d1c92b88a00b00c700a17cb3df8f0231add85
William Stein <wstein@sagemath.com>
2022-07-28 05:48:18 -0700

    adding emcc

--- packages/examples/wasm-memory-2/a.js
+++ packages/examples/wasm-memory-2/a.js
@@ -8,7 +8,7 @@ const table = new WebAssembly.Table({ initial: 10, element: "anyfunc" });
 function showTable() {
   v = [];
   for (let i = 0; i < table.length; i++) {
-    v.push(table.get(i) != null);
+    v.push(table.get(i) != null ? 1 : 0);
   }
   console.log(v);
 }
@@ -23,6 +23,7 @@ const mod = new WebAssembly.Module(typedArray);
 const instance = new WebAssembly.Instance(mod, opts);
 console.log("instance.exports = ", instance.exports);
 showTable();
+console.log("table.get(1)(4) = ", table.get(1)(4));
 
 
 const typedArray2 = new Uint8Array(require("fs").readFileSync("b.wasm"));
@@ -30,12 +31,13 @@ const mod2 = new WebAssembly.Module(typedArray2);
 opts.env = { ...instance.exports };
 opts.env.memory = memory;
 opts.env.__indirect_function_table = table;
+opts.env.__table_base = 4;
 const instance2 = new WebAssembly.Instance(mod2, opts);
 console.log("instance2.exports = ", instance2.exports);
 //table.set(0, instance2.exports.deref_in_b);
 
-console.log("next(10) = ", instance.exports.next(10));
 showTable();
+console.log("table.get(1)(4) = ", table.get(1)(4));
 
 module.exports = { instance, instance2 };
 
