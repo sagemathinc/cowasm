2f027de8b7095fee4cdb95111641570e45823a98
William Stein <wstein@sagemath.com>
2022-08-06 15:07:43 -0700

    eliminate some redundancy in the dylink makefile

--- packages/posix-wasm/src/posix-wasm.c
+++ packages/posix-wasm/src/posix-wasm.c
@@ -1,7 +1,12 @@
+#include "posix-wasm.h"
+
 #define STUB(x) printf("posix-wasm C STUB: %s\n", x);
-//#define STUB(x)
 char* strsignal(int sig) {
   STUB("strsignal");
   return "a signal";
 }
 
+gid_t getgid(void) { return 0; }
+gid_t getegid(void) { return 0; }
+uid_t getuid(void) { return 0; }
+uid_t geteuid(void) { return 0; }
