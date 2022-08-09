e00696a8e93494c82eac187455fe26de52e64a47
William Stein <wstein@sagemath.com>
2022-08-09 12:29:09 -0700

    add more pthread stubs and also get rid of stub warn exclusions, as they are dangerous

--- packages/posix-wasm/src/threads.c
+++ packages/posix-wasm/src/threads.c
@@ -6,15 +6,15 @@ pthreads (that used to be possible long ago).
 
 Inspired by https://github.com/mpdn/unthread
 
-NOTE: I had initially carried over a minimal implementation of pthread_mutex_init,
-but that actually breaks in Python/ceval_gil.h in the function
-create_gil, where the line MUTEX_INIT(gil->switch_mutex) somehow makes
+NOTE: I had initially carried over a minimal implementation of
+pthread_mutex_init, but that actually breaks in Python/ceval_gil.h in the
+function create_gil, where the line MUTEX_INIT(gil->switch_mutex) somehow makes
 it so the gil never gets set.    That's fine - this is not meant to work
 like actual pthreads yet!
 
 
 NOTE: Right after I spent like two days writing this something very
-similar popped up in Python itself!   https://github.com/python/cpython/pull/95234
+similar popped up in Python itself! https://github.com/python/cpython/pull/95234
 ARGH, that's so annoying, but obviously also good.  Their implementation is more
 minimal, but maybe that is better.  That said, these don't build due to
 incompatibility with libc-wasm-zig, so we're sticking with our own pthreads.
@@ -66,7 +66,15 @@ int pthread_cond_init(pthread_cond_t *cond, const pthread_condattr_t *attr) {
 }
 PUBLIC(pthread_cond_init)
 
-// The pthread_cond_signal() call unblocks at least one of the threads that are blocked on the specified condition variable cond (if any threads are blocked on cond).
+int pthread_cond_destroy(pthread_cond_t *cond) {
+  debug("pthread_cond_destroy - c implementation\n");
+  return 0;
+}
+PUBLIC(pthread_cond_destroy)
+
+// The pthread_cond_signal() call unblocks at least one of the threads that are
+// blocked on the specified condition variable cond (if any threads are blocked
+// on cond).
 int pthread_cond_signal(pthread_cond_t *cond) {
   debug("pthread_cond_signal - c implementation\n");
   // just do nothing - since we never block
@@ -106,7 +114,8 @@ void *pthread_getspecific(pthread_key_t key) {
 PUBLIC(pthread_getspecific)
 
 int pthread_setspecific(pthread_key_t key, const void *value) {
-  debug("pthread_setspecific - c implementation, key=%d, value=%p\n", key.id, value);
+  debug("pthread_setspecific - c implementation, key=%d, value=%p\n", key.id,
+        value);
   for (int i = 0; i < nkeys; i++) {
     if (keys[i].id == key.id) {
       debug("pthread_setspecific - changing key number %d\n", i);
@@ -131,10 +140,7 @@ int pthread_key_create(pthread_key_t *key, void (*destructor)(void *)) {
 
   static size_t next_key_id = 1;
 
-  do {
-    id = next_key_id++;
-  } while (id == 0);
-
+  id = next_key_id++;
   *key = (pthread_key_t){
       .id = id,
       .destructor = destructor,
@@ -145,8 +151,18 @@ int pthread_key_create(pthread_key_t *key, void (*destructor)(void *)) {
 }
 PUBLIC(pthread_key_create)
 
+int pthread_key_delete(pthread_key_t key) {
+  debug("pthread_key_delete - c implementation\n");
+  // we don't store anything, so nothing to delete...
+  return 0;
+}
+PUBLIC(pthread_key_delete)
 
-// The pthread_mutex_init() function initialises the mutex referenced by mutex with attributes specified by attr. If attr is NULL, the default mutex attributes are used; the effect is the same as passing the address of a default mutex attributes object. Upon successful initialisation, the state of the mutex becomes initialised and unlocked.
+// The pthread_mutex_init() function initialises the mutex referenced by mutex
+// with attributes specified by attr. If attr is NULL, the default mutex
+// attributes are used; the effect is the same as passing the address of a
+// default mutex attributes object. Upon successful initialisation, the state of
+// the mutex becomes initialised and unlocked.
 int pthread_mutex_init(pthread_mutex_t *mutex,
                        const pthread_mutexattr_t *attr) {
   debug("pthread_mutex_init - c implementation\n");
@@ -154,18 +170,34 @@ int pthread_mutex_init(pthread_mutex_t *mutex,
 }
 PUBLIC(pthread_mutex_init)
 
-// The mutex object referenced by mutex is locked by calling pthread_mutex_lock(). If the mutex is already locked, the calling thread blocks until the mutex becomes available. This operation returns with the mutex object referenced by mutex in the locked state with the calling thread as its owner.
+int pthread_mutex_destroy(pthread_mutex_t *mutex) {
+  debug("pthread_mutex_init - c implementation\n");
+  return 0;
+}
+PUBLIC(pthread_mutex_destroy)
+
+// The mutex object referenced by mutex is locked by calling
+// pthread_mutex_lock(). If the mutex is already locked, the calling thread
+// blocks until the mutex becomes available. This operation returns with the
+// mutex object referenced by mutex in the locked state with the calling thread
+// as its owner.
 int pthread_mutex_lock(pthread_mutex_t *mutex) {
-  //debug("pthread_mutex_lock - c implementation\n");
+  // debug("pthread_mutex_lock - c implementation\n");
   return 0;
 }
 PUBLIC(pthread_mutex_lock)
 
 int pthread_mutex_unlock(pthread_mutex_t *mutex) {
-  //debug("pthread_mutex_unlock - c implementation\n");
+  // debug("pthread_mutex_unlock - c implementation\n");
   return 0;
 }
 PUBLIC(pthread_mutex_unlock)
+
+int pthread_mutex_trylock(pthread_mutex_t *mutex) {
+  // debug("pthread_mutex_unlock - c implementation\n");
+  return 0;
+}
+PUBLIC(pthread_mutex_trylock)
 
 // The pthread_self() function returns the thread ID of the calling thread.
 pthread_t pthread_self() {
