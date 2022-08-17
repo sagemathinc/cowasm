81a4f7ce7a7bfb88aee7038bd914b38ede062843
William Stein <wstein@sagemath.com>
2022-08-16 18:11:41 -0700

    conditional compilation of login_tty

--- packages/posix-zig/src/other.zig
+++ packages/posix-zig/src/other.zig
@@ -1,18 +1,25 @@
 const c = @import("c.zig");
 const node = @import("node.zig");
-const util = @cImport(@cInclude("utmp.h"));
 const std = @import("std");
+const builtin = @import("builtin");
 
 pub fn register(env: c.napi_env, exports: c.napi_value) !void {
-    try node.registerFunction(env, exports, "login_tty", login_tty);
+    if (builtin.target.os.tag == .linux) {
+        try node.registerFunction(env, exports, "login_tty", login_tty);
+    }
 }
 
 // int login_tty(int fd);
 fn login_tty(env: c.napi_env, info: c.napi_callback_info) callconv(.C) c.napi_value {
-    const argv = node.getArgv(env, info, 1) catch return null;
-    const uid = node.i32FromValue(env, argv[0], "fd") catch return null;
-    if (util.login_tty(uid) == -1) {
-        node.throwError(env, "error in login_tty");
+    if (builtin.target.os.tag == .linux) {
+        const util = @cImport(@cInclude("utmp.h"));
+        const argv = node.getArgv(env, info, 1) catch return null;
+        const uid = node.i32FromValue(env, argv[0], "fd") catch return null;
+        if (util.login_tty(uid) == -1) {
+            node.throwError(env, "error in login_tty");
+        }
+        return null;
+    } else {
+        node.throwError(env, "login_tty not supported on this platform");
     }
-    return null;
 }
