b16fe7566d422f4746e99ab5b242ea3bbd5b58b1
William Stein <wstein@sagemath.com>
2022-08-18 19:30:54 -0700

    add a new "random python tests" file with a currently failing test (that should pass)

---
+++ packages/python-wasm/src/python/misc.test.ts
@@ -0,0 +1,16 @@
+import { exec, init, repr } from "./node";
+
+beforeEach(async () => {
+  await init({ noWorker: true });
+});
+
+test("that creating a directory works, and can get isting on new directory", async () => {
+  const path = `/tmp/${Math.random()}`;
+  try {
+    await exec(`import os; os.makedirs('${path}')`);
+    expect(await repr(`os.path.exists('${path}')`)).toBe("True");
+    expect(await repr(`os.listdir('${path}')`)).toBe("[]");
+  } finally {
+    await exec(`os.unlink('${path}')`);
+  }
+});
