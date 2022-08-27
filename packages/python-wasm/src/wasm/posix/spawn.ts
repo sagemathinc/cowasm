0db9ca2ab83648955d7e1654640e80c93cfa6ca4
William Stein <wstein@sagemath.com>
2022-08-26 19:01:01 -0700

    merge conflict (?)

--- packages/python-wasm/src/wasm/posix/spawn.ts
+++ packages/python-wasm/src/wasm/posix/spawn.ts
@@ -12,13 +12,12 @@ export default function spawn({}) {
     "posix_spawn posix_spawn_file_actions_addclose posix_spawn_file_actions_adddup2 posix_spawn_file_actions_addopen posix_spawn_file_actions_destroy posix_spawn_file_actions_init posix_spawnattr_destroy posix_spawnattr_init posix_spawnattr_setflags posix_spawnattr_setpgroup posix_spawnattr_setschedparam posix_spawnattr_setschedpolicy posix_spawnattr_setsigdefault posix_spawnattr_setsigmask posix_spawnp";
   const spawn: any = {};
   for (const name of names.split(" ")) {
-    spawn[name] = ()  = {
-      console.warn(`\n******\ncalling non-implemented stub function ${name}\n******\n`);
-      > {
-      console.warn(`\n******\ncalling non-implemented stub function ${name}\n******\n`);
+    spawn[name] = () => {
+      console.warn(
+        `\n******\ncalling non-implemented stub function ${name}\n******\n`
+      );
       notImplemented(name);
-      };
-  };
+    };
   }
   return spawn;
 }
