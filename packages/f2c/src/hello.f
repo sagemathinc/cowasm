dfe47e1f3785ff27bfdadc45f8c299e528f093e7
William Stein <wstein@sagemath.com>
2022-09-09 09:22:13 -0700

    add f2c package

---
+++ packages/f2c/src/hello.f
@@ -0,0 +1,28 @@
+      program main
+
+c*********************************************************************72
+c
+cc MAIN is the main program for HELLO.
+c
+c  Discussion:
+c
+c    HELLO is a simple FORTRAN77 program that says "Hello, world!".
+c
+c  Licensing:
+c
+c    This code is distributed under the GNU LGPL license.
+c
+c  Modified:
+c
+c    18 May 2009
+c
+c  Author:
+c
+c    John Burkardt
+c
+      implicit none
+
+      write ( *, '(a)' ) '  Hello, world!'
+
+      stop
+      end
