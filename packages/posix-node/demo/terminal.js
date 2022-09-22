9949ffa55a5d2b3ea00970a8f9df548190a5b454
William Stein <wstein@sagemath.com>
2022-09-21 17:43:02 -0700

    add demo; update package version

---
+++ packages/posix-node/demo/terminal.js
@@ -0,0 +1,12 @@
+const posix = require("..");
+console.log(
+  "This is a little terminal that capitalizes whatever you type, as you type it."
+);
+console.log("Hit ctrl+c or ctrl+d to exit, and of course this is not burning cpu while waiting for input.");
+console.log("You can usé utf-8: 😀");
+
+while (true) {
+  const t = posix.getChar();
+  if (t.charCodeAt(0) == 4) break;
+  process.stdout.write(t.toUpperCase());
+}
