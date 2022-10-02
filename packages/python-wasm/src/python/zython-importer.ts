911fafd1f71773d9b2dd5d31586e4398ac75223c
William Stein <wstein@sagemath.com>
2022-10-01 20:16:33 -0700

    put packages in py subdirectory to better organize them

--- packages/python-wasm/src/python/zython-importer.ts
+++ packages/python-wasm/src/python/zython-importer.ts
@@ -8,8 +8,8 @@ export default async function initZythonImporter(): Promise<string> {
   const modules: { [name: string]: string } = {};
   for (const name of MODULES) {
     for (const path of [
-      join(__dirname, `${name}.zip`),
-      join(__dirname, `${name}.tar.xz`),
+      join(__dirname, "py", `${name}.zip`),
+      join(__dirname, "py", `${name}.tar.xz`),
       join(packages, `py-${name}/dist/wasm/${name}.zip`),
       join(packages, `py-${name}/dist/wasm/${name}.tar.xz`),
     ]) {
