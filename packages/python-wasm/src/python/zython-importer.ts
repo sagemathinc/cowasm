0749fb2b2183018850b9ee43e2f96e07f173cbac
William Stein <wstein@sagemath.com>
2022-10-02 07:44:34 -0700

    made cython into a zython package (tar.xz file)
    - this is broken right now (e.g., the cython script)
      because I haven't finished implementing importing
      submodules in zython_importer.py

--- packages/python-wasm/src/python/zython-importer.ts
+++ packages/python-wasm/src/python/zython-importer.ts
@@ -1,23 +1,15 @@
 import { join } from "path";
 import { existsSync } from "fs";
-const MODULES = ["mpmath", "sympy", "numpy"];
+import { readdir } from "fs/promises";
+
+const modulePath = join(__dirname, "py");
 
 // Return string that gets evaluated in Python to initialize the zython importer.
 export default async function initZythonImporter(): Promise<string> {
-  const packages = join(__dirname, "../../..");
   const modules: { [name: string]: string } = {};
-  for (const name of MODULES) {
-    for (const path of [
-      join(__dirname, "py", `${name}.zip`),
-      join(__dirname, "py", `${name}.tar.xz`),
-      join(packages, `py-${name}/dist/wasm/${name}.zip`),
-      join(packages, `py-${name}/dist/wasm/${name}.tar.xz`),
-    ]) {
-      if (existsSync(path)) {
-        modules[name] = path;
-        break;
-      }
-    }
+  if (!existsSync(modulePath)) return "";
+  for (const name of await readdir(modulePath)) {
+    modules[name.split(".")[0]] = join(modulePath, name);
   }
   return `import zython_importer; zython_importer.install(${JSON.stringify(
     modules
