91dbfffd034ccf9596917ca2f26fb3369d9d1180
William Stein <wstein@sagemath.com>
2022-10-07 14:40:35 -0700

    wrong import source

--- packages/python-wasm/src/wasm/posix/wait.zig
+++ packages/python-wasm/src/wasm/posix/wait.zig
@@ -1,1 +1,4 @@
-pub const constants = .{ .c_import = @cImport(@cInclude("sys/wait.h")), .names = [_][:0]const u8{ "WNOHANG", "WUNTRACED" } };
+// These constants are actually only defined by us in posix-wasm.h, since they
+// aren't part of WASI at all.
+
+pub const constants = .{ .c_import = @cImport(@cInclude("posix-wasm.h")), .names = [_][:0]const u8{ "WNOHANG", "WUNTRACED" } };
