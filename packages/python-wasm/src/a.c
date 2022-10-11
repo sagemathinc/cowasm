e957ff1c55833c0a2420daa879cb0566cc91f297
William Stein <wstein@sagemath.com>
2022-10-11 08:43:17 -0700

    trying to understand options for running binaries

--- packages/python-wasm/src/a.c
+++ packages/python-wasm/src/a.c
@@ -8,6 +8,19 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+
+/*
+zig wasm-ld --error-limit=0 -O2 -s --stack-first --export-dynamic -z stack-size=1048576 --allow-undefined -o a.wasm /Users/wstein/build/cocalc/src/data/projects/2c9318d1-4f8b-4910-8da7-68a965514c95/.cache/zig/o/19455a81af1d577095ab969d4bb235f4/crt1-command.o /Users/wstein/build/cocalc/src/data/projects/2c9318d1-4f8b-4910-8da7-68a965514c95/.cache/zig/o/05c39cfef9dda29a79d3d9258afe0aa3/libc.a ./a.o /Users/wstein/build/cocalc/src/data/projects/2c9318d1-4f8b-4910-8da7-68a965514c95/.cache/zig/o/16da3be53de3230043688a5b4fec38fc/libcompiler_rt.a
+*/
+
+// extern void keepalive(size_t size);
+// WASM_EXPORT(keepalive, keepalive)
+
+__attribute__((visibility("default"))) void* c_malloc(size_t n) {
+  return malloc(n);
+}
+__attribute__((visibility("default"))) void c_free(void* p) { free(p); }
 
 unsigned long long sum(int n) {
   unsigned long long s = 0;
@@ -32,7 +45,7 @@ long long time0() {
 extern char* user_from_uid(uid_t uid, int nouser);
 
 int main(int argc, char** argv) {
-
+  printf("%p\n",c_malloc(0));
   for (int i = 0; i < argc; i++) {
     printf("argv[%d]=%s\n", i, argv[i]);
   }
