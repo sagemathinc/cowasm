51dc2c70a05ff36bda187686625ed6d6bb5bff97
William Stein <wstein@sagemath.com>
2022-10-14 08:04:41 -0700

    working on coreutils

--- packages/python-wasm/src/wasm/posix/util.ts
+++ packages/python-wasm/src/wasm/posix/util.ts
@@ -1,3 +1,6 @@
+import debug from "debug";
+const log = debug("posix");
+
 export class NotImplementedError extends Error {
   ret: number;
   constructor(functionName: string, ret?: number) {
@@ -9,6 +12,8 @@ export class NotImplementedError extends Error {
   }
 }
 
-export function notImplemented(functionName: string, ret?: number) {
+export function notImplemented(functionName: string, ret: number = -1) {
+  console.warn("WARNING: calling NOT IMPLEMENTED function", functionName);
+  log("WARNING: calling NOT IMPLEMENTED function", functionName);
   throw new NotImplementedError(functionName, ret);
 }
