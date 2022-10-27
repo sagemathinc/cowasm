f12b12ba97738721b58f4c1ba02fd61d8e94fa6f
William Stein <wstein@sagemath.com>
2022-10-27 16:14:36 -0700

    node: not using a worker by default
    - as much as possible, we want to avoid worker for node version,
    since that will avoid subtle issues with posix extensions.

--- packages/python-wasm/src/node-terminal.ts
+++ packages/python-wasm/src/node-terminal.ts
@@ -1,5 +1,5 @@
 /*
-python-wasm [--no-bundle] [--no-worker] ...
+python-wasm [--no-bundle] [--worker] ...
 */
 
 import { resolve } from "path";
@@ -8,39 +8,39 @@ import { Options } from "./common";
 
 async function main() {
   const PYTHONEXECUTABLE = resolve(process.argv[2]);
-  const { noBundle, noWorker } = processArgs(process.argv);
+  const { noBundle, worker } = processArgs(process.argv);
   const options: Options = { env: { PYTHONEXECUTABLE }, interactive: true };
   if (!noBundle) {
     options.fs = "everything";
   }
-  const Python = noWorker ? syncPython : asyncPython;
+  const Python = worker ? asyncPython : syncPython;
   const python = await Python(options);
   const argv = [PYTHONEXECUTABLE].concat(process.argv.slice(3));
   const r = await python.terminal(argv);
   if (argv.includes("-h")) {
-    console.log("\npython-wasm [--no-worker] [--no-bundle] ...");
+    console.log("\npython-wasm [--worker] [--no-bundle] ...");
     console.log(
-      "--no-worker : use single threaded version, instead of a worker thread"
+      "--worker : use a worker thread, instead of single threaded version"
     );
     console.log(
-      "--no-bundle : do not use the python library bundle archive (only for development)"
+      "--no-bundle : do not use the python bundle archive (only for development)"
     );
   }
   process.exit(r);
 }
 
-function processArgs(argv: string[]): { noBundle: boolean; noWorker: boolean } {
+function processArgs(argv: string[]): { noBundle: boolean; worker: boolean } {
   const i = argv.indexOf("--no-bundle");
   const noBundle = i != -1;
   if (noBundle) {
     argv.splice(i, 1);
   }
-  const j = argv.indexOf("--no-worker");
-  const noWorker = j != -1;
-  if (noWorker) {
+  const j = argv.indexOf("--worker");
+  const worker = j != -1;
+  if (worker) {
     argv.splice(j, 1);
   }
-  return { noBundle, noWorker };
+  return { noBundle, worker };
 }
 
 main();
