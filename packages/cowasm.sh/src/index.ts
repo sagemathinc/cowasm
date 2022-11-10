2f11817c31e167961fc7de6a7ab62e03a1a86ce1
William Stein <wstein@sagemath.com>
2022-11-09 22:07:59 -0800

    fix favicon
    - I had it correct for 64x64 but it also had smaller incorrect ones embedded

--- packages/cowasm.sh/src/index.ts
+++ packages/cowasm.sh/src/index.ts
@@ -3,7 +3,7 @@ import favicon from "./favicon.ico";
 
 const link = document.createElement("div");
 link.innerHTML =
-  `<div style='text-align:center;margin-bottom:15px;color:#555'><h3><img width="24px" height="24px" src="${favicon}" style="float:left; margin-left:15px"/> CoWasm.sh &nbsp;&nbsp;-&nbsp;&nbsp;<a target='_blank' style='text-decoration: none;' href='https://www.npmjs.com/package/dash-wasm'>npm</a>&nbsp;&nbsp;&nbsp;<a target='_blank' style='text-decoration: none;' href='https://github.com/sagemathinc/cowasm#readme'/>GitHub</a></h3>CoWasm: Collaborative WebAssembly for servers and browsers.<br/>This is a demo of the <a target='_blank' href='https://www.npmjs.com/package/dash-wasm'>shell component</a> of CoWasm.  There is also a <a target='_blank' href='https://cowasm.org'>Python demo</a>.<br/></div>`;
+  `<div style='text-align:center;margin-bottom:15px;color:#555'><h3><img width="32px" height="32px" src="${favicon}" style="float:left; margin-left:15px"/><img width="32px" height="32px" src="${favicon}" style="float:right; margin-right:15px"/> CoWasm.sh &nbsp;&nbsp;-&nbsp;&nbsp;<a target='_blank' style='text-decoration: none;' href='https://www.npmjs.com/package/dash-wasm'>npm</a>&nbsp;&nbsp;&nbsp;<a target='_blank' style='text-decoration: none;' href='https://github.com/sagemathinc/cowasm#readme'/>GitHub</a></h3>CoWasm: Collaborative WebAssembly for servers and browsers.<br/>This is a demo of the <a target='_blank' href='https://www.npmjs.com/package/dash-wasm'>shell component</a> of CoWasm.  There is also a <a target='_blank' href='https://cowasm.org'>Python demo</a>.<br/></div>`;
 document.body.appendChild(link);
 
 const favlink = document.createElement("link");
--- packages/website/src/index.ts
+++ packages/website/src/index.ts
@@ -3,7 +3,7 @@ import favicon from "./favicon.ico";
 
 const link = document.createElement("div");
 link.innerHTML =
-  `<div style='text-align:center;margin-bottom:15px;color:#555'><h3><img width="24px" height="24px" src="${favicon}" style="float:left; margin-left:15px"/> CoWasm Python-Wasm Demo&nbsp;&nbsp;-&nbsp;&nbsp;<a target='_blank' style='text-decoration: none;' href='https://www.npmjs.com/package/python-wasm'>npm</a>&nbsp;&nbsp;&nbsp;<a target='_blank' style='text-decoration: none;' href='https://github.com/sagemathinc/cowasm#readme'/>GitHub</a></h3>CoWasm: Collaborative WebAssembly for servers and browsers.  An alternative to Emscripten that is built using <a  target='_blank' href='https://ziglang.org/'>Zig</a>.<br/>There is also a <a href='https://cowasm.sh' target='_blank'>CoWasm Shell demo</a>.</div>`;
+  `<div style='text-align:center;margin-bottom:15px;color:#555'><h3><img width="32px" height="32px" src="${favicon}" style="float:left; margin-left:15px"/><img width="32px" height="32px" src="${favicon}" style="float:right; margin-right:15px"/> CoWasm Python-Wasm Demo&nbsp;&nbsp;-&nbsp;&nbsp;<a target='_blank' style='text-decoration: none;' href='https://www.npmjs.com/package/python-wasm'>npm</a>&nbsp;&nbsp;&nbsp;<a target='_blank' style='text-decoration: none;' href='https://github.com/sagemathinc/cowasm#readme'/>GitHub</a></h3>CoWasm: Collaborative WebAssembly for servers and browsers.  An alternative to Emscripten that is built using <a  target='_blank' href='https://ziglang.org/'>Zig</a>.<br/>There is also a <a href='https://cowasm.sh' target='_blank'>CoWasm Shell demo</a>.</div>`;
 document.body.appendChild(link);
 const element = document.createElement("div");
 document.body.appendChild(element);
