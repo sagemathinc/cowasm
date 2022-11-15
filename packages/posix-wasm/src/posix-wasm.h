5c6f902fb3f7dcbaa6aa18cea5a2c8477a7a191d
William Stein <wstein@sagemath.com>
2022-11-15 15:16:22 -0800

    another attempt to define setjmp stubs...

--- packages/posix-wasm/src/posix-wasm.h
+++ packages/posix-wasm/src/posix-wasm.h
@@ -270,7 +270,13 @@ int close_range(unsigned int first, unsigned int last, unsigned int flags);
 
 // These are not needed by Python but are needed by PARI (which isn't
 // even part of CoWasm!?):
-#include <setjmp.h>
+typedef void* __jmp_buf;
+typedef struct __jmp_buf_tag {
+	__jmp_buf __jb;
+	unsigned long __fl;
+	unsigned long __ss[128/sizeof(long)];
+} jmp_buf[1];
+typedef jmp_buf sigjmp_buf;
 //typedef void* jmp_buf;
 //typedef void* sigjmp_buf;
 int setjmp(jmp_buf env);
