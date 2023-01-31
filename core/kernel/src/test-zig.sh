abedc9253a34c9a15276a9ca49c105cf273b1538
William Stein <wstein@sagemath.com>
2023-01-31 09:50:12 -0800

    kernel had an implicit dep on python

--- core/kernel/src/test-zig.sh
+++ core/kernel/src/test-zig.sh
@@ -8,5 +8,5 @@ export TEST_CMD="$SRC"/../node_modules/.bin/wasi-run
 export TARGET="$1"
 shift
 
-echo zig test -target wasm32-wasi --test-cmd "$TEST_CMD" --test-cmd-bin  -I. -I"$CPYTHON_WASM/include/python3.11" -I"$POSIX_WASM" -L"$CPYTHON_WASM/lib" -lpython3.11 -lc  --main-pkg-path "$SRC" `pwd`/$TARGET "$@"
-zig test -target wasm32-wasi --test-cmd "$TEST_CMD" --test-cmd-bin  -I. -I"$CPYTHON_WASM/include/python3.11" -I"$POSIX_WASM" -L"$CPYTHON_WASM/lib" -lpython3.11 -lc  --main-pkg-path "$SRC" `pwd`/$TARGET "$@"
+echo zig test -target wasm32-wasi --test-cmd "$TEST_CMD" --test-cmd-bin  -I. -I"$POSIX_WASM" -lc  --main-pkg-path "$SRC" `pwd`/$TARGET "$@"
+zig test -target wasm32-wasi --test-cmd "$TEST_CMD" --test-cmd-bin  -I. -I"$POSIX_WASM" -lc --main-pkg-path "$SRC" `pwd`/$TARGET "$@"
